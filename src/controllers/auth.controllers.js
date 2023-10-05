45822e3f7635ca70d394b889071f9e4838b5b6b4
thevillas <villalbas991@gmail.com>
2023-10-05 17:50:17 -0500

    mongo_register

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,28 @@
 import user from '../models/user.models.js';
 
-export const register = (req, res) => res.send("register");
+export const register = async (req, res) => {
+    //desestructurar el body que se envia
+    const{ email, password, username} = req.body;
+
+    try {
+        const newUser = new user({
+            username,
+            email,
+            password
+        });
+
+        const userSave = await newUser.save();
+        return res.status(201).json(userSave);
+
+
+    } catch (error) {
+        res.status(500).json({ menssage: error.menssage});
+    }
+};
+
+
+
+
+
+
 export const login = (req, res) => res.send("login");
--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1/mernlogin");
+        await mongoose.connect("mongodb+srv://villalbas991:megustaprogramar23@cluster0.s5epy4z.mongodb.net/react_crud_proyect");
         console.log(">>DB connect")
     } catch (error) {
         console.log(error)
