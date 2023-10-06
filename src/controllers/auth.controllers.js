afcefdf1a22c65047d812e305d1d043ce49ff67a
thevillas <villalbas991@gmail.com>
2023-10-05 19:28:07 -0500

    parte_uno

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,22 +1,32 @@
-import user from '../models/user.models.js';
+import user from '../models/user.models.js'
+import bcrypt from 'bcryptjs';
+import { createTokenAccess } from '../libs/jwt.js';
+import { token } from 'morgan';
 
 export const register = async (req, res) => {
     //desestructurar el body que se envia
     const{ email, password, username} = req.body;
 
     try {
+        const passwordHash = await bcrypt.hash(password, 10);
         const newUser = new user({
             username,
             email,
-            password
+            password:passwordHash
         });
 
         const userSave = await newUser.save();
-        return res.status(201).json(userSave);
+        const Token = await createTokenAccess({ id: userSave._id });
+        res.cookie('token', Token);
+        return res.status(201).json({
+            id: userSave._id,
+            username: userSave.username,
+            email: userSave.email
+        });
 
 
     } catch (error) {
-        res.status(500).json({ menssage: error.menssage});
+        res.status(500).json({ message: error.message});
     }
 };
 
